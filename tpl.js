89b3eb68e069a8be147f1e1bc30ae89f728736cf
nerdfiles <nerdfiles@gmail.com>
2018-10-31 21:31:57 -0500

    Fix for isBuild configuration

--- tpl.js
+++ tpl.js
@@ -6,18 +6,18 @@
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
- *     1. Redistributions of source code must retain the above copyright notice, 
+ *
+ *     1. Redistributions of source code must retain the above copyright notice,
  *        this list of conditions and the following disclaimer.
- *     
- *     2. Redistributions in binary form must reproduce the above copyright 
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright
  *        notice, this list of conditions and the following disclaimer in the
  *        documentation and/or other materials provided with the distribution.
- * 
+ *
  *     3. Neither the name of David Hall nor the names of its contributors may be
  *        used to endorse or promote products derived from this software without
  *        specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -38,36 +38,49 @@ define([
 
     "use strict";
 
+    function loadFile(path) {
+      if (typeof process !== "undefined" && process.versions && !!process.versions.node && require.nodeRequire) {
+        var fs = require.nodeRequire('fs');
+        var file = fs.readFileSync(path, 'utf8');
+        if (file.indexOf('\uFEFF') === 0)
+          return file.substring(1);
+        return file;
+      }
+    }
+
     var tplModule = null;
-    
+
     var buildMap = {};
 
     var tpl = {
 
-        _cacheTemplate: function(angular, name, contents) {
-            if (!tplModule) {
-                tplModule = angular.module("tpl", []);
-            }
-            tplModule.run(["$templateCache", function($templateCache) {
-                $templateCache.put(name, contents);
-            }]);
+        _cacheTemplate: function(name, contents) {
+            require(['angular'], function(angular) {
+              if (!tplModule) {
+                  tplModule = angular.module("tpl", []);
+              }
+              tplModule.run(["$templateCache", function($templateCache) {
+                  $templateCache.put(name, contents);
+              }]);
+            });
             return contents;
         },
 
         load: function(name, parentRequire, onload, config) {
-            text.get(require.toUrl("../templates/" + name), function(contents) {
-                // How we act next depends on whether this is the browser.
-                if (config.isBuild) {
-                    // This is an optimizing build
-                    buildMap[name] = contents;
-                    onload(contents);
-                } else {
+            if (!config.isBuild) {
+                text.get(require.toUrl("../templates/" + name), function(contents) {
                     // Add to the template cache.
                     require(["angular"], function(angular) {
                         onload(tpl._cacheTemplate(angular, name, contents));
                     });
-                }
-            });
+                });
+            } else {
+                var fileName = require.toUrl(name);
+                buildMap[name] = tpl._cacheTemplate(name, loadFile(fileName));
+            }
+
+            onload();
+
         },
 
         write: function (pluginName, moduleName, write) {
